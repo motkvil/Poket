ba9dbfdaf7ab9c1ae32e5740bbe3eb709f31f5e3
luisalexis2021 <119871038+motkvil@users.noreply.github.com>
2023-10-11 19:18:19 +0200

    FloatWindow

---
+++ src/assets/db/data.jsx
@@ -0,0 +1,309 @@
+let items = [
+  {
+    title: 'Alojamiento',
+    items: [
+      {
+        title:'Hipoteca o alquiler',
+        prev_cost:0,
+        real_cost:0
+      },
+      {
+        title:'Telefono',
+        prev_cost:0,
+        real_cost:0
+      },{
+        title:'Electricidad',
+        prev_cost:0,
+        real_cost:0
+      },{
+        title:'Gas',
+        prev_cost:0,
+        real_cost:0
+      },{
+        title:'Agua y alcantarillado',
+        prev_cost:0,
+        real_cost:0
+      },{
+        title:'Television por cable',
+        prev_cost:0,
+        real_cost:0
+      },{
+        title:'Recogida de residuos',
+        prev_cost:0,
+        real_cost:0
+      },{
+        title:'Mantenimiento o reparaciones',
+        prev_cost:0,
+        real_cost:0
+      },{
+        title:'Suministros',
+        prev_cost:0,
+        real_cost:0
+      },{
+        title:'Otros',
+        prev_cost:0,
+        real_cost:0
+      }
+      
+      
+      
+    ]
+  },{
+    title: 'Transporte',
+    items: [
+      {
+        title:'Pago del vehiculo',
+        prev_cost:0,
+        real_cost:0
+      },{
+        title:'Gastos de taxi o bus',
+        prev_cost:0,
+        real_cost:0
+      },{
+        title:'Seguro',
+        prev_cost:0,
+        real_cost:0
+      },{
+        title:'Licencias',
+        prev_cost:0,
+        real_cost:0
+      },{
+        title:'Combustible',
+        prev_cost:0,
+        real_cost:0
+      },{
+        title:'Mantenimiento',
+        prev_cost:0,
+        real_cost:0
+      },{
+        title:'Otros',
+        prev_cost:0,
+        real_cost:0
+      }
+    ]
+  },{
+    title: 'Seguro',
+    items: [
+      {
+        title:'Hogar',
+        prev_cost:0,
+        real_cost:0
+      },{
+        title:'Salud',
+        prev_cost:0,
+        real_cost:0
+      },{
+        title:'Vida',
+        prev_cost:0,
+        real_cost:0
+      },{
+        title:'Otros',
+        prev_cost:0,
+        real_cost:0
+      }
+    ]
+  },{
+    title: 'Comida',
+    items: [
+      {
+        title:'Alimentos',
+        prev_cost:0,
+        real_cost:0
+      },{
+        title:'Restaurantes',
+        prev_cost:0,
+        real_cost:0
+      },{
+        title:'Otros',
+        prev_cost:0,
+        real_cost:0
+      }
+    ]
+  },{
+    title: 'Mascotas',
+    items: [
+      {
+        title:'Comida',
+        prev_cost:0,
+        real_cost:0
+      },{
+        title:'Medicos',
+        prev_cost:0,
+        real_cost:0
+      },{
+        title:'Limpieza',
+        prev_cost:0,
+        real_cost:0
+      },{
+        title:'Juguetes',
+        prev_cost:0,
+        real_cost:0
+      },{
+        title:'Otros',
+        prev_cost:0,
+        real_cost:0
+      }
+    ]
+  },{
+    title: 'Cuidado Personal',
+    items: [
+      {
+        title:'Medicos',
+        prev_cost:0,
+        real_cost:0
+      },{
+        title:'Pelo y uñas',
+        prev_cost:0,
+        real_cost:0
+      },{
+        title:'Ropa',
+        prev_cost:0,
+        real_cost:0
+      },{
+        title:'Tintorería',
+        prev_cost:0,
+        real_cost:0
+      },{
+        title:'Gimnasio',
+        prev_cost:0,
+        real_cost:0
+      },{
+        title:'Tasas o cuotas de la organizacion',
+        prev_cost:0,
+        real_cost:0
+      },{
+        title:'Otros',
+        prev_cost:0,
+        real_cost:0
+      }
+    ]
+  },{
+    title: 'Entretenimiento',
+    items:[
+      {
+        title:'Noche de salir',
+        prev_cost:0,
+        real_cost:0
+      },{
+        title:'Plataformas de musica',
+        prev_cost:0,
+        real_cost:0
+      },{
+        title:'Peliculas',
+        prev_cost:0,
+        real_cost:0
+      },{
+        title:'Conciertos',
+        prev_cost:0,
+        real_cost:0
+      },{
+        title:'Eventos deportivos',
+        prev_cost:0,
+        real_cost:0
+      },{
+        title:'Teatro',
+        prev_cost:0,
+        real_cost:0
+      },{
+        title:'Otros',
+        prev_cost:0,
+        real_cost:0
+      }
+    ]
+  },{
+    title: 'Prestamos',
+    items: [
+      {
+        title:'Personal',
+        prev_cost:0,
+        real_cost:0
+      },{
+        title:'Bancario',
+        prev_cost:0,
+        real_cost:0
+      },{
+        title:'Tarjeta de credito',
+        prev_cost:0,
+        real_cost:0
+      },{
+        title:'Adelanto de nomina',
+        prev_cost:0,
+        real_cost:0
+      }
+    ]
+  },{
+    title: 'Impuestos',
+    items: [
+      {
+        title:'Federales',
+        prev_cost:0,
+        real_cost:0
+      },{
+        title:'Estatales',
+        prev_cost:0,
+        real_cost:0
+      },{
+        title:'Locales',
+        prev_cost:0,
+        real_cost:0
+      },{
+        title:'Otros',
+        prev_cost:0,
+        real_cost:0
+      }
+    ]
+  },{
+    title: 'Ahorros o Inversiones',
+    items: [
+      {
+        title:'Cuenta de jubilación',
+        prev_cost:0,
+        real_cost:0
+      },{
+        title:'Cuenta de inversion',
+        prev_cost:0,
+        real_cost:0
+      },{
+        title:'Otros',
+        prev_cost:0,
+        real_cost:0
+      }
+    ]
+  },{
+    title: 'Regalos y Donaciones',
+    items: [
+      {
+        title:'Organizacion benefica 1',
+        prev_cost:0,
+        real_cost:0
+      },{
+        title:'Organizacion benefica 2',
+        prev_cost:0,
+        real_cost:0
+      },{
+        title:'Organizacion benefica 3',
+        prev_cost:0,
+        real_cost:0
+      }
+    ]
+  },{
+    title: 'Legal',
+    items: [
+      {
+        title:'Abogados',
+        prev_cost:0,
+        real_cost:0
+      },{
+        title:'Pensión alimenticia',
+        prev_cost:0,
+        real_cost:0
+      },{
+        title:'Pagos por retencion o fallo',
+        prev_cost:0,
+        real_cost:0
+      }
+    ]
+  }
+]
+
+export default items
---
+++ src/assets/floatWindow.jsx
@@ -0,0 +1,87 @@
+import items from "./db/data"
+import { Box, Grid } from "@mui/material"
+
+const FloatWindow = (props) => {
+
+  let item = items.find(item => item.title === props.itemActive)
+
+  return (
+    <Box
+      position={'fixed'} boxSizing={'border-box'}
+      bgcolor={'hsla(0,0%,0%,.1)'}
+      p={1} height={'100%'} left={0} top={0}width={'100%'}
+      display={'flex'} justifyContent={'center'} alignItems={'center'}
+    >
+
+      <Box
+         height={'80vh'}
+        p={1} width={'80vw'} borderRadius={2}
+      >
+
+
+        <Box height={'90%'} bgcolor={'white'} borderRadius={3}
+          overflow={'auto'}
+        >
+          <Box className={'itemDetails'}>
+            <Box
+              bgcolor={'#ff0000'} color={'white'} fontSize={20}
+              style={{borderTopLeftRadius:'10px',borderTopRightRadius:'10px'}} p={1}
+            >
+              <Grid container>
+                <Grid item xs={5}>
+                  <strong>{item.title}</strong>
+                </Grid>
+                
+
+                <Grid item xs={4}>
+                  <strong>Costo previsto</strong>
+                </Grid>
+                <Grid item xs={3}>
+                  <strong>Costo real</strong>
+                </Grid>
+              </Grid>
+            </Box>
+
+            <Box>
+              {
+                item.items.map((item,index)=>(
+                  <Box key={index} p={1} fontSize={20} display={'flex'} borderBottom={1} color={"hsla(0,0%,0%,.2)"}>
+                    <Grid container>
+                      <Grid item xs={5}>
+                        <Box fontSize={20} color={'black'} p={1}><strong>{item.title}</strong></Box>
+                      </Grid>
+                      <Grid item xs={4}>
+                        <Box p={1} bgcolor={'hsla(39,60%,60%,.9)'} borderRadius={3} color={'black'}>${item.prev_cost}</Box>
+                      </Grid>
+                      <Grid item xs={3}>
+                        <Box p={1} color={'black'} >${item.real_cost}</Box>
+                      </Grid>
+                    </Grid>
+                  </Box>
+                ))
+              }
+            </Box>
+          </Box>
+        </Box>
+
+        <Box display={'flex'} p={1} justifyContent={'space-around'} textAlign={'center'}>
+          <Box bgcolor={'red'} borderRadius={1} minWidth={100} p={1}
+            fontSize={20} color={'white'} style={{cursor:'pointer'}} onClick={()=>props.setItemIsActive(false)}
+          >
+            <strong>Salir</strong>
+          </Box>
+
+          <Box bgcolor={'red'} borderRadius={1} minWidth={100} p={1}
+            fontSize={20} color={'white'} style={{cursor:'pointer'}}
+          >
+            <strong>Agregar item</strong>
+          </Box>
+        </Box>
+
+      </Box>
+
+    </Box>
+  )
+}
+
+export default FloatWindow
